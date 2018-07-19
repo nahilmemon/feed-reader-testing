56cffcc0a7e8c846f2075cff737e4b80cdea74e4
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-07-19 22:23:12 +0400

    test: Test the names in the allFeeds variable

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,11 +31,15 @@ $(function() {
       }
     });
 
-
-    /* TODO: Write a test that loops through each feed
-     * in the allFeeds object and ensures it has a name defined
-     * and that the name is not empty.
+    /* Test: Make sure that each feed in the allFeeds variable
+     * has a defined name and that this name is not empty.
      */
+    it('names are defined', function() {
+      for (const feed of allFeeds) {
+        expect(feed['name']).toBeDefined();
+        expect(feed['name'].length).not.toBe(0);
+      }
+    });
   });
 
 
