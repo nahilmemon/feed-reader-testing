2447121b5c619b51d99273d6ac1e9b1377548b8e
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-07-19 23:57:30 +0400

    test: Check if .feed isn't empty after loadFeed()

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -70,12 +70,21 @@ $(function() {
   /* Test suite: test that the initial entries array isn't empty
    * after the loadFeed() function finishes executing. */
   describe('Initial Entries', function() {
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+    /* Make an asynchronous request for loadFeed() to execute and finish
+     * before testing its contents. */
+    beforeEach(function(done) {
+      loadFeed(0, function() {
+        done();
+      });
+    });
+
+    /* Test: After loadFeed() finishes executing, there is at least one
+     * .entry element within the .feed container div. */
+    it('contains at least one entry after loadFeed() finishes executing', function(done) {
+      const CONTAINER = document.querySelector('.feed');
+      expect(CONTAINER.querySelectorAll('.entry').length).toBeGreaterThan(0);
+      done();
+    });
   });
 
   /* TODO: Write a new test suite named "New Feed Selection" */
